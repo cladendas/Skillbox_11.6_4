8fa399e24e0450f96f754ac14c1fa2041609e3f7
Vadim <rvdagi05@yandex.ru>
2022-10-12 14:25:35 +0300

    Создание функции "проверка на кол-во побед в игре и определение победителя" и изменение кода в соответсвии с ней, изменение функции "сравнение символов"

--- main.cpp
+++ main.cpp
@@ -1,82 +1,120 @@
 #include <iostream>
 
 //сравнение символов
-bool compare(char valid, char sym) {
-    return valid == sym;
+char compare(char symF, char symS, char symT) {
+    char wonChar = 'N';
+    if (symF == 'X' && symS == 'X' && symT == 'X') {
+        wonChar = 'X';
+    } else if (symF == 'O' && symS == 'O' && symT == 'O') {
+        wonChar = 'O';
+    }
+    return wonChar;
+}
+
+//проверка на кол-во побед в игре и определение победителя
+char checkWin(int countX, int countO) {
+    char wonChar = 'N';
+    if (countX > 1 || countO > 1 || ((countX == countO) && countX != 0)) {
+        wonChar = 'I';
+    } else if (countX == 1) {
+        wonChar = 'X';
+    } else if (countO == 1) {
+        wonChar = 'O';
+    }
+    return wonChar;
 }
 
 //проверка диагонали на победителя
-std::string checkDiag(std::string str) {
-    std::string win = "";
+char checkDiag(std::string str) {
+    char wonChar;
+    //кол-во побед Х
+    int countX = 0;
+    //кол-во побед О
+    int countO = 0;
 
     for (int i = 0; i <= 2; i += 2) {
-        if (compare('X', str[i])
-            && compare('X', str[i + 4])
-            && compare('X', str[i + 8])) {
-
-            win = "Petya";
-        } else if (compare('O', str[i])
-                   && compare('O', str[i + 4])
-                   && compare('O', str[i + 8])) {
-
-            win = "Vanya";
+        if (i == 0) {
+            char tmp = compare(str[i], str[i + 4], str[i + 8]);
+            if (tmp == 'X') {
+                countX++;
+            } else if (tmp == 'O') {
+                countO++;
+            }
+        }
+        if (i == 2) {
+            char tmp = compare(str[i], str[i + 2], str[i + 4]);
+            if (tmp == 'X') {
+                countX++;
+            } else if (tmp == 'O') {
+                countO++;
+            }
         }
     }
-    return win;
+    wonChar = checkWin (countX, countO);
+    return wonChar;
 }
 
 //проверка столбцов на победителя
-std::string checkWonCol(std::string str) {
-    std::string win = "";
+char checkWonCol(std::string str) {
+    char wonChar;
+    //кол-во побед Х
+    int countX = 0;
+    //кол-во побед О
+    int countO = 0;
     for (int i = 0; i < 3; i++) {
-        if (compare('X', str[i])
-            && compare('X', str[i + 3])
-            && compare('X', str[i + 6])) {
-
-            win = "Petya";
-        } else if (compare('O', str[i])
-                   && compare('O', str[i + 3])
-                   && compare('O', str[i + 6])) {
-
-            win = "Vanya";
+        char tmp = compare(str[i], str[i + 4], str[i + 8]);
+        if (tmp == 'X') {
+            countX++;
+        } else if (tmp == 'O') {
+            countO++;
         }
     }
-    return win;
+    wonChar = checkWin (countX, countO);
+    return wonChar;
 }
 
 //проверка строки на победителя
-std::string checkWonStr(std::string str) {
-    std::string win = "";
+char checkWonStr(std::string str) {
+    char wonChar;
+    //кол-во побед Х
+    int countX = 0;
+    //кол-во побед О
+    int countO = 0;
     for (int i = 0; i <= 6 ; i += 3) {
-        if (compare('X', str[i])
-            && compare('X', str[i + 1])
-            && compare('X', str[i + 2])) {
-
-            win = "Petya";
-        } else if (compare('O', str[i])
-                   && compare('O', str[i + 1])
-                   && compare('O', str[i + 2])) {
-
-            win = "Vanya";
+        char tmp = compare(str[i], str[i + 4], str[i + 8]);
+        if (tmp == 'X') {
+            countX++;
+        } else if (tmp == 'O') {
+            countO++;
         }
     }
-    return win;
+    wonChar = checkWin (countX, countO);
+    return wonChar;
 }
 
 //проверка строки на общие требования
 bool checkStringTotal(std::string str) {
-    if (str.length() != 3) {
+    //кол-во Х
+    int countX = 0;
+    //кол-во О
+    int countO = 0;
+    if (str.length() != 9) {
         return false;
     } else {
-        for (int i = 0; i < str.length(); i++) {
-            if (compare('X', str[i])
-                || compare('O', str[i])
-                || compare('.', str[i])) {
-                    return false;
-            } else {
-                    return true;
+        for (char i : str) { //сравниваем введённые символы с допустимыми символами
+            if (i == 'X') {
+                countX++;
+            } else if (i == 'O') {
+                countO++;
+            } else if (i != '.') {
+                return false;
             }
         }
+
+        if (countX < countO) { //кол-во Х должно быть больше или равно кол-ву О
+            return false;
+        }
+        return true;
     }
 }
 
@@ -84,41 +122,58 @@ void checkTotal(std::string fStr, std::string sStr, std::string tStr) {
 
     std::string totalStr = fStr + sStr + tStr;
 
-    //проверка первой строки на общие требования
-    bool checkStringTotalF = checkStringTotal(totalStr);
+    //проверка строк на общие требования
+    bool checkStrTotal = checkStringTotal(totalStr);
 
     //получение победителя по строкам
-    std::string checkWonStrings = checkWonStr(totalStr);
+    char checkWonStrings = checkWonStr(totalStr);
 
     //получение победителя по столбцам
-    std::string checkWonColumns = checkWonCol(totalStr);
+    char checkWonColumns = checkWonCol(totalStr);
 
     //получение победителя по диагоналям
-    std::string checkDiagWon = checkDiag(totalStr);
+    char checkDiagWon = checkDiag(totalStr);
 
 
-    if () {
-
-
-        std::cout << "Incorrect";
-    } else if () {
-        std::cout << "Vanya won";
-    } else if () {
-
-        std::cout << "Petya won";
-    } else if () {
-        std::cout << "Nobody";
+    if (checkStrTotal) {
+        if (checkWonStrings == 'X' || checkWonColumns == 'X' || checkDiagWon == 'X') {
+            std::cout << "Petya\n";
+        } else if (checkWonStrings == 'O' || checkWonColumns == 'O' || checkDiagWon == 'O') {
+            std::cout << "Vanya\n";
+        } else if (checkWonStrings == 'N' || checkWonColumns == 'N' || checkDiagWon == 'N') {
+            std::cout << "Nobody\n";
+        }
+    } else {
+        std::cout << "Incorrect\n";
     }
-}
-
-void checkAll() {
-
 }
 
 int main() {
     std::string fStr, sStr, tStr;
-    std::cout << "Enter result of game:" << std::endl;
-    std::cin >> fStr >> sStr >> tStr;
+//    std::cout << "Enter result of game:" << std::endl;
+//    std::cin >> fStr >> sStr >> tStr;
+
+    checkTotal("X..",
+               "OXO",
+               "OOO");
+    checkTotal("X..",
+               ".X.",
+               "OO.");
+    checkTotal("XXO",
+               "OOX",
+               "XOX");
+    checkTotal("XO.",
+               "XO.",
+               "X.O");
+    checkTotal("OX.",
+               "XOX",
+               "X.O");
+    checkTotal("..X",
+               "OX.",
+               "X.O");
+    checkTotal("0..",
+               "...",
+               "...");
 
     return 0;
 }
